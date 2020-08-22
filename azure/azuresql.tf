753396513a32db2c8d7da6227ff5a8119400f2e9
Kapil <kapilsree@gmail.com>
2020-08-22 19:23:52 +0000

    Azure Sql terraform scripts

---
+++ azure/azuresql.tf
@@ -0,0 +1,50 @@
+provider "azurerm" {
+  version = "=2.2.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "dataflow" {
+  name     = "hybridclouddataflow"
+  location = "West US"
+}
+
+resource "azurerm_sql_server" "dataflow" {
+  name                         = "dataflowsqlserver"
+  resource_group_name          = azurerm_resource_group.dataflow.name
+  location                     = "West US"
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_storage_account" "dataflow" {
+  name                     = "dataflowsa"
+  resource_group_name      = azurerm_resource_group.dataflow.name
+  location                 = azurerm_resource_group.dataflow.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_database" "dataflow" {
+  name                = "dataflowsqldatabase"
+  resource_group_name = azurerm_resource_group.dataflow.name
+  location            = "West US"
+  server_name         = azurerm_sql_server.dataflow.name
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.dataflow.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.dataflow.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+
+
+
+  tags = {
+    environment = "dev"
+  }
+}
