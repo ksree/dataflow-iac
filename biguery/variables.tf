c59b9a6d01903b96e8833bba7bb22569611e3baf
Kapil <kapilsree@gmail.com>
2020-08-23 19:27:12 +0000

    cleanup folder structure

--- biguery/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {
-  description = "Project where the dataset is  created."
-}
-
-variable "delete_contents_on_destroy" {
-  description = "(Optional) If set to true, delete all the tables in the dataset when destroying the resource; otherwise, destroying the resource will fail if tables are present."
-  type        = bool
-  default     = null
-}
