f63392b1b26b4e58a455295aae3c4974a1f1b2bf
Kapil <kapilsree@gmail.com>
2020-08-23 17:59:12 +0000

    Create redshift terraform

--- aws/redshift.tf
+++ aws/redshift.tf
@@ -2,11 +2,11 @@ provider "aws" {
   version = "~> 3.0"
   region  = "us-east-2"
 }
-yesource "aws_redshift_cluster" "dataflow_resource" {
+resource "aws_redshift_cluster" "dataflow_resource" {
   cluster_identifier = "dataflow-redshift-cluster"
   database_name      = "dataflow"
-  master_username    = "4dm1n157r470r"
-  master_password    = "4-v3ry-53cr37-p455w0rd"
+  master_username    = "adm1n157r470r"
+  master_password    = "OdfdPdeanfioefn232dedw"
   node_type          = "dc1.large"
   cluster_type       = "single-node"
 }
