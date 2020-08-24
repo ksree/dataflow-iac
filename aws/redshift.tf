9933c1197d5a68c6eb823228c21cd7d77a79c1c9
Kapil Sreedharan <kapilsreed@gmail.com>
2020-08-24 14:47:18 +0200

    update node type to dc2.large

--- aws/redshift.tf
+++ aws/redshift.tf
@@ -7,6 +7,6 @@ resource "aws_redshift_cluster" "dataflow_resource" {
   database_name      = "dataflow"
   master_username    = "adm1n157r470r"
   master_password    = "OdfdPdeanfioefn232dedw"
-  node_type          = "dc1.large"
+  node_type          = "dc2.large"
   cluster_type       = "single-node"
 }
