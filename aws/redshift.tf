efc2ee8eceaae1b45b68080c2499628a8f8ef715
Kapil <kapilsree@gmail.com>
2020-08-26 17:34:38 +0000

    created tf variables for aws and azure

--- aws/redshift.tf
+++ aws/redshift.tf
@@ -3,10 +3,11 @@ provider "aws" {
   region  = "us-east-2"
 }
 resource "aws_redshift_cluster" "dataflow_resource" {
-  cluster_identifier = "dataflow-redshift-cluster"
-  database_name      = "dataflow"
-  master_username    = "adm1n157r470r"
-  master_password    = "OdfdPdeanfioefn232dedw"
-  node_type          = "dc2.large"
-  cluster_type       = "single-node"
+  cluster_identifier  = "dataflow-redshift-cluster"
+  database_name       = "dataflow"
+  master_username     = "adm1n157r470r"
+  master_password     = "OdfdPdeanfioefn232dedw"
+  node_type           = "dc2.large"
+  cluster_type        = "single-node"
+  skip_final_snapshot = true
 }
---
+++ azure/variables.tf
@@ -0,0 +1,8 @@
+variable "start_ip_address" {
+  type = string
+}
+
+variable "end_ip_address" {
+  type = string
+}
+
